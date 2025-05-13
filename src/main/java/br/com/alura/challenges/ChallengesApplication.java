2277b59f2858b37bda6ceff34814aa8d18cc2d39
Marlon_Vieira <marlon.vieira@viasoft.com.br>
2025-05-13 17:09:41 -0300

    Challenge 01, 02 and 03. 13/05/2025

--- src/main/java/br/com/alura/challenges/ChallengesApplication.java
+++ src/main/java/br/com/alura/challenges/ChallengesApplication.java
@@ -1,13 +1,26 @@
 package br.com.alura.challenges;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Scanner;
+
 @SpringBootApplication
-public class ChallengesApplication {
+public class ChallengesApplication implements CommandLineRunner  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChallengesApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Scanner scan = new Scanner(System.in);
+
+		System.out.printf("Enter a number to do the count: ");
+		Integer number = scan.nextInt();
+		for (int i = 1; i <= number; i++) {
+			System.out.printf(i + " - ");
+		}
+	}
 }
